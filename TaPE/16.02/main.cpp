823e0c416e74a988a9781eb296cb2c4eab0a589d
Katy248 <petrovanton247@gmail.com>
2024-03-01 11:16:02 +0300

    Update code

--- TaPE/16.02/main.cpp
+++ TaPE/16.02/main.cpp
@@ -1,21 +1,38 @@
 #include <iostream>
-#include <cstdlib>;
-#include <windows.h>;
+#include <cstdlib>
 #include <algorithm>
+
+/* Windows specific lib */
+#if __WIN32__
+#include <windows.h>
+#endif
 using namespace std;
 
 int main()
 {
+    const unsigned int MESSAGE_LENGTH = 80;
+    
+    /* If current OS is Windows it will work with russian letters othervise only with english */
+    #if __WIN32__
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    int i = 0, k;
     char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
-    char msg[80];
+    #else
+    char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    #endif
+
+    
+    int 
+        i = 0, 
+        k;
+    char msg[MESSAGE_LENGTH];
+
     cout << "Enter message: " << endl;
-    cin.getline(msg, 80);
+    cin.getline(msg, MESSAGE_LENGTH);
     cout << endl << "Enter offset:" << endl;
     cin >> k;
     cout << endl;
+    
     char* begin = alphabet;
     char* end = alphabet + sizeof(alphabet);
     for (; msg[i]; ++i)
