6b2e4aa33f82680a21bf257af4a755988f285952
Katy248 <petrovanton247@gmail.com>
2024-03-02 11:41:04 +0300

    Finish

--- TaPE/01.03/1/main.cpp
+++ TaPE/01.03/1/main.cpp
@@ -4,83 +4,75 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-double **get_input(int[]);
-double *get_full_weights(double **, int *);
-double *get_baggage_with_average_differ_on(double **, double);
-int get_passengers_count_with_more_than(double **, double);
-
-int main()
+int main(int argc, char **argv)
 {
-    int *sizes;
-    double **baggages = get_input(sizes);
-    for (size_t i = 0; i < sizeof(sizes) / sizeof(int); i++)
-    {
-        cout << sizes[i] << endl;
-    }
-    
+    bool interactive = argv[1] == "-i";
+    if (!interactive)
+        cout << "[INFO] NON_INTERACTIVE" << endl;
 
-    /* 1 task */
-    double *full_weights = get_full_weights(baggages, sizes);
-    for (size_t i = 0; i < sizeof(full_weights) / sizeof(double); i++)
-    {
-        cout << i << "th passanger baggage weight = " << full_weights[i] << endl;
-    }
-
-    /* 2 task */
-    // double *bgs = get_baggage_with_average_differ_on(baggages, 0.3);
-    // for (size_t i = 0; i < sizeof(bgs) / sizeof(double); i++)
-    // {
-    //     cout << bgs[i] << endl;
-    // }
-
-    // /* 3 task */
-    // cout << "Passengers with more than 3 things in baggage is " << get_passengers_count_with_more_than(baggages, 3);
-
-    return 0;
-}
-
-double **get_input(int sizes[])
-{
     int baggage_count;
-    cout << "Enter baggages count: ";
+
+    if (interactive)
+        cout << "Enter baggages count: ";
     cin >> baggage_count;
-    cout << endl;
+
     double **baggages = new double *[baggage_count];
-    sizes = new int[baggage_count];
+    double *weight_by_passenger = new double[baggage_count];
+    int *items_by_passenger = new int[baggage_count];
+    int more_than_3_count = 0;
 
     for (int i = 0; i < baggage_count; i++)
     {
-        int passenge_baggages_count;
-        cout << "Enter " << i << "th passengers baggages count: ";
-        cin >> passenge_baggages_count;
-        cout << endl;
-        baggages[i] = new double[passenge_baggages_count];
-        sizes[i] = passenge_baggages_count;
 
-        for (int j = 0; j < passenge_baggages_count; j++)
+        baggages[i] = new double[items_by_passenger[i]];
+
+        if (interactive)
+            cout << "Enter " << i << "th passengers baggages count: ";
+        cin >> items_by_passenger[i];
+
+        if (items_by_passenger[i] > 3)
         {
-            cout << "Enter weight: ";
+            more_than_3_count++;
+        }
+
+        weight_by_passenger[i] = 0;
+        for (int j = 0; j < items_by_passenger[i]; j++)
+        {
+            if (interactive)
+                cout << "Enter weight: ";
             cin >> baggages[i][j];
-            cout << endl;
+
+            weight_by_passenger[i] += baggages[i][j];
         }
     }
-
-    return baggages;
-}
-double *get_full_weights(double **baggages, int *sizes)
-{
-    unsigned int length = sizeof(sizes) / sizeof(int);
-    
-    double *weights = new double[length];
-    for (size_t i = 0; i < length; i++)
+    /* 1st */
+    cout << "All weights:" << endl;
+    for (int i = 0; i < baggage_count; i++)
     {
-        double weight = 0;
-        for (size_t j = 0; j < sizeof(baggages[i]) / sizeof(double); j++)
-        {
-            weight += baggages[i][j];
-        }
-        weights[i] = weight;
+        cout << "\t" << weight_by_passenger[i] << endl;
     }
 
-    return weights;
+    /* 2nd */
+    int all_items_count = 0;
+    double all_weight = 0;
+    for (int i = 0; i < baggage_count; i++)
+    {
+        all_items_count += items_by_passenger[i];
+        all_weight += weight_by_passenger[i];
+    }
+
+    double average_weight = all_weight / all_items_count;
+
+    for (int i = 0; i < baggage_count; i++)
+    {
+        if (weight_by_passenger[i] / items_by_passenger[i] - average_weight <= 0.3)
+        {
+            cout << "Baggage #" << i << ": baggage avg: " << weight_by_passenger[i] / items_by_passenger[i] << "; all avg: " << average_weight << endl;
+            break;
+        }
+    }
+
+    /* 3rd */
+    cout << "Passengers with more than 3 items in baggages\n"
+         << "\t" << more_than_3_count;
 }
