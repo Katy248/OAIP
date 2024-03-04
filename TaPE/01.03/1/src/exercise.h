ca0493880feea68fe115f84f1587039fd80ce66f
Katy248 <petrovanton247@gmail.com>
2024-03-04 16:09:19 +0300

    Remove unnecessary files

--- TaPE/01.03/1/src/exercise.h
+++
@@ -1,5 +0,0 @@
-#define u32 unsigned int
-
-void get_weight_by_passengers(u32, u32 **, double ***, double **);
-u32 get_index_of_passenger(u32, u32 **, double ***);
-u32 get_passengers_count_with_items_more_than(u32, u32 **, double ***, u32);
