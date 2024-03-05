d1dd3836ddccce460383b432218c5a466e4e66a4
Katy248 <petrovanton247@gmail.com>
2024-03-05 12:16:03 +0300

    Add solution

---
+++ OAIP/05.03/2/encrypt/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using std::cerr;
+using std::cin;
+using std::clog;
+using std::cout;
+using std::endl;
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        cerr << "Not enough args" << endl;
+        return 1;
+    }
+
+    unsigned int value = std::stoi(argv[1]);
+
+    if (value < 1000 || value > 9999)
+    {
+        cerr << "Input value not of 4 digits" << endl;
+        return 2;
+    }
+
+    int digits[4];
+
+    for (int i = 0; i < 4; i++)
+    {
+        digits[4 - 1 - i] = value % 10; /* get digit */
+        digits[4 -1 - i] = (digits[4 -1 - i] + 7) % 10; /* encrypt digit */
+        value /= 10;
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+        cout << digits[i]; 
+    }
+    cout << endl;
+
+    return 0;
+}
