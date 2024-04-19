34a27f3148def35439df1c28027b6dde80677786
Katy248 <petrovanton247@gmail.com>
2024-04-19 13:44:44 +0300

    Refactor code

--- OAIP/16.04/9/main.py
+++ OAIP/16.04/9/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 
+
 def string_contains(string: str, value: str) -> bool:
     for sym in value:
         if sym in string:
@@ -10,31 +11,34 @@ def string_contains(string: str, value: str) -> bool:
 def check_passwd(password: str) -> (bool,):
     errors = []
     if (len(password) < 8):
-        errors.append(f"Password is too short ({len(password)} symbols). Should be 8 or more symbols")
+        errors.append(
+            f"Password is too short ({len(password)} symbols). Should be 8 or more symbols")
     if (password.islower()):
-        errors.append("No capitalized symbols. Password should contains at least 1 capitalized character")
-    if(not string_contains(password, "0123456789")):
+        errors.append(
+            "No capitalized symbols. Password should contains at least 1 capitalized character")
+    if (not string_contains(password, "0123456789")):
         errors.append("No numeric symbols. Password should contains digits")
     if (not string_contains(password, "*-#")):
         errors.append("Password doesn't contains special symbols.")
-    
-        
 
     return (len(errors) == 0, errors)
+
 
 def main():
     if len(sys.argv) > 1:
         password = sys.argv[1]
     else:
-        print("No args specified")
+        print("No args specified", file=sys.stderr)
         exit(1)
-    (is_success, errors) = check_passwd(password)
 
+    (is_success, errors) = check_passwd(password)
     if (is_success):
         print("Password is perfect")
     else:
         for err in errors:
             print(err, file=sys.stderr)
+        exit(1)
+
 
 if __name__ == "__main__":
     main()
